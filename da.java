732c3b0cf900769c8418152cd5c81e2951769eb6
김민지 <84611998+kimminji-1130@users.noreply.github.com>
2023-02-09 16:06:51 +0900

    Stack_VPS
    괄호 문자열 스택써서 해결하기

--- da.java
+++ da.java
@@ -1,34 +1,42 @@
 package babu;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class da {
 
-	public static void main(String[] args) {
-		  Scanner sc = new Scanner(System.in);
-		  int[][] a = new int[8][2];
-		  int[] a1 = new int[5];
-		  int sum=0;
-		  for(int i=0;i<8;i++) {
-			  a[i][0] = sc.nextInt();
-			  a[i][1] = i+1;
-		  }
-		  Arrays.sort(a, new Comparator<int[]>() {
-				@Override
-				public int compare(int[] a, int[] b) {
-					return b[0] - a[0];
-				}
-			});
-		  for(int i=0;i<5;i++) {
-			  sum += a[i][0];
-				a1[i] = a[i][1];
-		  }
-		  Arrays.sort(a1);
-		  
-		  
-		  System.out.println(sum);
-		  for(int i=0; i<5;i++) {
-			  System.out.print(a1[i]+" ");
-		  }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Character> stack = new Stack<>();
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			int length = str.length();
 
+			for (int j = 0; j < length; j++) {
+				char ch = str.charAt(j);
+
+				if (ch == '(') {
+					stack.push(ch);
+				} else {
+					int size = stack.size();
+					if (size == 0) {
+						stack.push(ch);
+						break;
+					} else {
+						stack.pop();
+					}
+				}
+			}
+
+			if (stack.isEmpty()) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
+
+			stack.clear();
+		}
 	}
 }
